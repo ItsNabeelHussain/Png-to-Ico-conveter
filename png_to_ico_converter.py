0aa2de26af9281b600d04a9c7bad10c4d5acf40f
Nabeel-Python-Developer <93263502+Nabeel-Python-Developer@users.noreply.github.com>
2021-11-01 11:40:22 +0500

    Update png_to_ico_converter.py

--- png_to_ico_converter.py
+++ png_to_ico_converter.py
@@ -1,9 +1,9 @@
 
-""" Pillow or PIL is a python image 
-processing library that provides extensive 
-file format support an efficient internal 
+""" Pillow or PIL is a python image
+processing library that provides extensive
+file format support an efficient internal
 representation, and fairly powerful image
-processing capabilities.it is fork from 
+processing capabilities.it is fork from
 Python Imaging Library(PIL)
 """
 import os
@@ -11,21 +11,34 @@ import os
 from PIL import Image
 
 
-def png_to_ico_converter():
+def png_to_ico_converter(image_file) -> None:
+
     """
-    fucntion for converting png format 
-    to ico format.It takes file from the 
-    user,convert it into ico and return 
-    icon  file 
+    fucntion for converting png format
+    to ico format.It takes file from the
+    user,convert it into ico and return
+    icon  file
     """
 
-    image_file = input("Choose image file from the directory")
-    if os.path.isfile(image_file):
-        file = Image.open(image_file)
-        icon_file = file.save('file.ico')
-        print("Successfully converted into required format")
-        
-    else:
-        print("Invalid File path")
- if __name__ == "__main__":
-    png_to_ico_converter()
+    try:
+        # Check the existing of the File
+        if os.path.isfile(image_file):
+         # Open the image file using Image class instance of the Pillow library
+            file = Image.open(image_file)
+            # Resizing the image
+            new_image = file.resize((400, 400))
+            # Saving image file in ICO format
+            new_image.save('icon_file.ico')
+            print("Successfully converted into required Format")
+        else:
+            print("Please select correct directory")
+
+    except FileNotFoundError:
+        print("image file does not exists")
+
+
+if __name__ == "__main__":
+    print("**** Welcome to the Png to Icon Converter ****")
+    image_file = input("Choose image file from the directory: ")
+
+    png_to_ico_converter(image_file)
